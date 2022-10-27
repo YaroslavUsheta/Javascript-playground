d6563199c18e36494c805b0d4f83dfc836831ac4
Dmytro Bevzenko <dbvz@protonmail.com>
2022-10-26 23:12:33 -0500

    Add check for digits

--- greeting-with-argv/run.js
+++ greeting-with-argv/run.js
@@ -1,11 +1,27 @@
 /**
+ * Return true if input contains digits.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+ * && AND
+ * || OR
+ */
+function containsDigits(str) {
+    return str.includes("0") || str.includes("1");
+}
+
+/**
  * Prepares the greeting based on the user input.
  * input --> message (string)
  * Note that console.log() is also a function. It return undefined.
+ * () parenthesis
+ * [] square brackets
+ * {} curly braces
  */
 function greeting(input) {
   if (!input) {
     return "enter the name";
+  }
+  if (containsDigits(input)) {
+    return "Digits are not allowed."
   }
   return "Hello, " + input;
 }
