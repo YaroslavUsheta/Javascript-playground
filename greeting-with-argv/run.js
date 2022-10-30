abba8558458c6d16c088627e1a578fda8de91447
Dmytro Bevzenko <dbvz@protonmail.com>
2022-10-30 18:58:25 -0500

    Finish greeting-with-args

--- greeting-with-argv/run.js
+++ greeting-with-argv/run.js
@@ -5,9 +5,19 @@
  * || OR
  */
 function containsDigits(str) {
-  return /0-9/.containsDigits(str);
+    // We are not going to use here RegEx (regular expressions)
+    // return /0-9/.containsDigits(str);
+    return str.includes("0") ||
+        str.includes("1") ||
+        str.includes("2") ||
+        str.includes("3") ||
+        str.includes("4") ||
+        str.includes("5") ||
+        str.includes("6") ||
+        str.includes("7") ||
+        str.includes("8") ||
+        str.includes("9");
 }
-console.log("No digits alowed");
 
 /**
  * Prepares the greeting based on the user input.
@@ -18,13 +28,13 @@ console.log("No digits alowed");
  * {} curly braces
  */
 function greeting(input) {
-  if (!input) {
-    return "enter the name";
-  }
-  if (containsDigits(input)) {
-    return "Digits are not allowed.";
-  }
-  return "Hello, " + input;
+    if (!input) {
+        return "enter the name";
+    }
+    if (containsDigits(input)) {
+        return "Digits are not allowed.";
+    }
+    return "Hello, " + input + ".";
 }
 
 // Prepare the message
