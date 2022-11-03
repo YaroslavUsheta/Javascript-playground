7e8f57de6252c5f4b3b5f4556b95d5d616b526c4
YaroslavUsheta <yusheta@gmail.comm>
2022-11-02 23:01:59 -0500

    looked over code, played with parseInt and parse Float

--- array-parsing/run.js
+++ array-parsing/run.js
@@ -11,26 +11,26 @@ let numbers = [];
 
 // for ( executes before loop starts ;  terminating condition (if false loop stops) ; executes after every loop iteration )
 for (let i = 2; i < process.argv.length; i++) {
-    // console.log(process.argv[i]);
+  //console.log(process.argv[i]);
 
-    const stringInTheInput = process.argv[i];
+  //const stringInTheInput = process.argv[i];
 
-    const numberInTheInput = parseFloat(stringInTheInput);
+  const numberInTheInput = parseFloat(stringInTheInput);
 
-    // adding process.argv[i] to the end of the array
-    numbers.push(numberInTheInput);
-    // console.log("After push", numbers);
+  // adding process.argv[i] to the end of the array
+  numbers.push(numberInTheInput);
+  console.log("After push", numbers);
 }
 
 // because Math.max does not take array, but need to get all numbers as function arguments,
 // we convert array in that using ... (array destructuring).
-const maxInTheInput = Math.max(...numbers);
+//const maxInTheInput = Math.max(...numbers);
 
-// console.log("Numbers", numbers);
+//console.log("Numbers", numbers);
 
 // const num = process.argv[3];
 
-console.log("Max number in the input is", maxInTheInput);
+//console.log("Max number in the input is", maxInTheInput);
 
 // Play with parseInt and parseFloat in the interactive node session.
 // Find parseInt and parseFloat in the book.
