b4bb43210778e369d1c50eb7c49a5a42117b1692
YaroslavUsheta <yusheta@gmail.comm>
2022-11-06 20:12:15 -0600

    learning progress

--- array-parsing/run.js
+++ array-parsing/run.js
@@ -1,4 +1,18 @@
 // parse numbers and find max
+console.log(process.argv);
+console.log(typeof process.argv);
+console.log(Array.isArray(process.argv));
+//console.log(process.argv.length);
+let c = 2;
+console.log(process.argv[c]);
+
+const fruits = ["apple", "banana", "pear"];
+
+const b = 45 - 44;
+
+const someFruit = fruits[b];
+
+console.log("The value of someFruit is", someFruit);
 
 // creates an empty array.
 let numbers = [];
@@ -14,17 +28,18 @@ for (let i = 2; i < process.argv.length; i++) {
   //console.log(process.argv[i]);
 
   const stringInTheInput = process.argv[i];
-
+  console.log(stringInTheInput);
+  process.exit();
   const numberInTheInput = parseFloat(stringInTheInput);
 
   // adding process.argv[i] to the end of the array
   numbers.push(numberInTheInput);
 
-  function containsLatters(num) {
-    if ([] === NaN) return (input + "numbers are not allowed")[i];
-  }
+  // function containsLatters(num) {
+  // if ([] === NaN) return (input + "numbers are not allowed")[i];
+  //}
 }
-// console.log("After push", numbers);
+console.log("After push", numbers);
 
 // because Math.max does not take array, but need to get all numbers as function arguments,
 // we convert array in that using ... (array destructuring).
