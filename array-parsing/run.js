a008ea3b07d60d4c7a6215b00ca958272a9e0765
YaroslavUsheta <yusheta@gmail.comm>
2022-11-08 14:47:38 -0600

    went over assignment

--- array-parsing/run.js
+++ array-parsing/run.js
@@ -1,18 +1,18 @@
 // parse numbers and find max
-console.log(process.argv);
-console.log(typeof process.argv);
-console.log(Array.isArray(process.argv));
+//console.log(process.argv);
+//console.log(typeof process.argv);
+//console.log(Array.isArray(process.argv));
 //console.log(process.argv.length);
-let c = 2;
-console.log(process.argv[c]);
+//let c = 2;
+//console.log(process.argv[c]);
 
-const fruits = ["apple", "banana", "pear"];
+//const fruits = ["apple", "banana", "pear"];
 
-const b = 45 - 44;
+//const b = 45 - 44;
 
-const someFruit = fruits[b];
+//const someFruit = fruits[b];
 
-console.log("The value of someFruit is", someFruit);
+//console.log("The value of someFruit is", someFruit);
 
 // creates an empty array.
 let numbers = [];
@@ -28,8 +28,8 @@ for (let i = 2; i < process.argv.length; i++) {
   //console.log(process.argv[i]);
 
   const stringInTheInput = process.argv[i];
-  console.log(stringInTheInput);
-  process.exit();
+  //console.log(stringInTheInput);
+
   const numberInTheInput = parseFloat(stringInTheInput);
 
   // adding process.argv[i] to the end of the array
@@ -44,6 +44,8 @@ console.log("After push", numbers);
 // because Math.max does not take array, but need to get all numbers as function arguments,
 // we convert array in that using ... (array destructuring).
 const maxInTheInput = Math.max(...numbers);
+
+//process.exit();
 
 //console.log("Numbers", numbers);
 
