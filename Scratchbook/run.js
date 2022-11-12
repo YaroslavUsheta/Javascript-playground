02cc8286913557225e3791d2fa9a806ef4885390
YaroslavUsheta <yusheta@gmail.comm>
2022-11-12 17:46:28 -0600

    added a scrathbook folder

---
+++ Scratchbook/run.js
@@ -0,0 +1,14 @@
+const id = {
+  firstName: "Scott",
+  lastName: "Hunter",
+  Title: "VP of Azure Development",
+
+  firstName: "Scott",
+  lastName: " Guthrie",
+  Title: "EVP of Azure & AI",
+
+  firstName: "Scott",
+  lastName: "Hanselman",
+  Title: "Director of Azure",
+};
+console.log(id);
