501060d55157ea8cc73b21305e105215064f0538
YaroslavUsheta <yusheta@gmail.comm>
2022-11-12 18:40:58 -0600

    problem solving

--- reading-csv/index.js
+++ reading-csv/index.js
@@ -1,3 +1,20 @@
+const id = {
+  firstName: "Scott",
+  lastName: "Hunter",
+  Title: "VP of Azure Development",
+}
+console.log(id)
+process.exit();
+  firstName: "Scott",
+  lastName: " Guthrie",
+  Title: "EVP of Azure & AI",
+
+  firstName: "Scott",
+  lastName: "Hanselman",
+  Title: "Director of Azure",
+};
+console.log(id);
+
 import { open } from "node:fs/promises";
 
 const file = await open("./users.csv");
