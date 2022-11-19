948f88c535bcba4adc2df4215006dbc5d78677e5
YaroslavUsheta <yusheta@gmail.comm>
2022-11-18 19:11:29 -0600

    working with string split

--- reading-csv/index.js
+++ reading-csv/index.js
@@ -1,7 +1,9 @@
 import { open } from "node:fs/promises";
 
 const file = await open("./users.csv");
+str.split(separator, limit);
 
 const fileContent = await file.readFile({ encoding: "utf8" });
 
 console.log(fileContent);
+//console.log()
