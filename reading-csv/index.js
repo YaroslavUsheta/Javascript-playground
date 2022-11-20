a2d7395f6d782d242e3a82bdda80c3de4026db49
YaroslavUsheta <yusheta@gmail.comm>
2022-11-19 20:33:08 -0600

    user data

--- reading-csv/index.js
+++ reading-csv/index.js
@@ -1,9 +1,34 @@
 import { open } from "node:fs/promises";
+import { listenerCount } from "node:process";
 
 const file = await open("./users.csv");
-str.split(separator, limit);
 
-const fileContent = await file.readFile({ encoding: "utf8" });
+const fileContent = awai line;
+for await (const line of file.readLines()) {
+      console.log(line);
+     }
+    
+    console.log(fileContent.split(lineBreak));
+    
+    const firstLine = fileContent.split(lineBreak)[1];
+    
+    console.log("Printing the first line");
+    
+    console.log(firstLine.split(','));
+    
+    const users = [
+      {
+        id: 1,
+        firstName: "Scott",
+        lastName: "Hunter",
+        title: "VP of Azure Development"
+      },
+      {
+        id: 1,
+        firstName: "Scott",
+        lastName: "Hunter",
+        title: "VP of Azure Development"
+      }
+    ]
 
-console.log(fileContent);
-//console.log()
+console.log(file);
