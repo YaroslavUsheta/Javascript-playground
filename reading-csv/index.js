e16a676f494f3d86fd3e3906bfe935f4ec615ce2
YaroslavUsheta <yusheta@gmail.comm>
2022-11-21 18:03:48 -0600

    spacifying user order

--- reading-csv/index.js
+++ reading-csv/index.js
@@ -11,25 +11,40 @@ const id = 2;
 const users = [];
 
 // for loop that populates the array
-for (let i = 1; i < lines.length; i++) {
-  console.log(`Running ${i} iteration of the for loop`, lines[i]);
-}
+//for (let i = 1; i < lines.length; i++) {
+//console.log(`Running ${i} iteration of the for loop`, lines[i]);
+//}
 
-process.exit();
-console.log(users[id]);
-
-/*
-
-console.log(fileContent);
-
+//console.log(users[id]);
 console.log(fileContent.split("\n"));
 
 const firstLine = fileContent.split("\n")[1];
 
-console.log("Printing the first line");
+console.log("First user");
 
-console.log(firstLine.split(","));use
+console.log(firstLine.split(","));
 
+const secondLine = fileContent.split("\n")[2];
+
+//console.log(fileContent.split("\n"));
+
+console.log("Second user");
+
+console.log(secondLine.split(","));
+
+//console.log(fileContent.split("\n"));
+
+const thirdLine = fileContent.split("\n")[3];
+
+console.log("Third user");
+
+console.log(thirdLine.split(","));
+
+use;
+
+process.exit();
+/*
+console.log(fileContent);
 */
 
 // node run start
