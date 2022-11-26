29063df065c09b73ddb98a5a218b5d1a4d74260a
YaroslavUsheta <yusheta@gmail.comm>
2022-11-26 02:32:56 -0600

    just experimenting

--- reading-csv/index.js
+++ reading-csv/index.js
@@ -6,40 +6,42 @@ const fileContent = await file.readFile({ encoding: "utf8" });
 
 const lines = fileContent.split("\n");
 
-const id = 2;
+//const id = 2;
 
-const users = [];
+//const users = [];
 
 // for loop that populates the array
-for (let i = 2; i < lines.length; i++) {
-  //console.log(`${i}st   list of users`, lines[i]);
-  console.log("list of users by", lines[0]);
-  console.log(fileContent.split("\n"));
+let i = 2;
+i < lines.length;
+i++;
+//console.log(`${i}st   list of users`, lines[i]);
+console.log("list of users by", lines[i]);
+console.log(fileContent.split("\n"));
 
-  const firstLine = fileContent.split("\n")[1];
+const firstLine = fileContent.split("\n")[1];
 
-  console.log("First user");
+console.log("First user");
 
-  console.log(firstLine.split(","));
+console.log(firstLine.split(","));
 
-  const secondLine = fileContent.split("\n")[2];
+const secondLine = fileContent.split("\n")[2];
 
-  //console.log(fileContent.split("\n"));
+//console.log(fileContent.split("\n"));
 
-  console.log("Second user");
+console.log("Second user");
 
-  console.log(secondLine.split(","));
+console.log(secondLine.split(","));
 
-  //console.log(fileContent.split("\n"));
+//console.log(fileContent.split("\n"));
 
-  const thirdLine = fileContent.split("\n")[3];
+const thirdLine = fileContent.split("\n")[3];
 
-  console.log("Third user");
+console.log("Third user");
 
-  console.log(thirdLine.split(","));
-}
+console.log(thirdLine.split(","));
+
 use;
-console.log(users[id]);
+//console.log(users[id]);
 
 process.exit();
 /*
