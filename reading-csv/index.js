9b8d831082adf03a34a4f8a55ef9759d4b05bdbe
Dmytro Bevzenko <dbvz@protonmail.com>
2022-12-11 22:24:45 -0600

    Add unfinished identifyTable function

--- reading-csv/index.js
+++ reading-csv/index.js
@@ -1,4 +1,8 @@
 import { open } from "node:fs/promises";
+import { EOL } from "node:os";
+
+// eventually we should ask a user to give us the filename to process
+const filename = "./users.csv";
 
 /**
  * Reads a given filename (if it exists) and returns array of file lines.
@@ -6,8 +10,7 @@ import { open } from "node:fs/promises";
 async function csvToArray(filename) {
   const file = await open(filename);
   const fileContent = await file.readFile({ encoding: "utf8" });
-  // line separator on Windows is "\r\n", but it is "\n" on Linux and macOS.
-  const lines = fileContent.split("\r\n");
+  const lines = fileContent.split(EOL);
   return lines;
 }
 
@@ -27,4 +30,51 @@ function lineToUser(line) {
 
 // }
 
-const lines = await csvToArray("./users.csv");
+/** For now we are going to handle only numbers and strings. */
+function typeOfColumn(lines, columnIndex) {
+  // start at 1 to skip the header
+  for (let i = 1; i < lines.length; i++) {
+    const columns = lines[i].split(',');
+    const requestedColumn = columns[columnIndex].trim();
+    // try to convert into the number
+    const maybeFloat = parseFloat(requestedColumn);
+    if (isNaN(maybeFloat)) {
+      return "varchar(255)";
+    }
+    const numberAsString = JSON.stringify(Math.round(maybeFloat));
+    if (numberAsString === requestedColumn) {
+      return "int";
+    } else {
+      return "double";
+    }
+  }
+}
+
+function identifyTable(lines) {
+  const header = lines[0];
+  const columns = header.split(','); 
+
+  // trim every element in the array
+  for (let i = 0; i < columns.length; i++) {
+    columns[i] = columns[i].trim();
+  }
+
+  // identify column data types
+  const firstColumnDataType = typeOfColumn(lines, 0);
+  console.log("Data type of the filrst column", firstColumnDataType);
+  
+  const output = [
+    {name: "id", type: "int"},
+    {name: "FirstName", type: "varchar(255)"},
+    {name: "LastName", type: "varchar(255)"},
+    {name: "Title", type: "varchar(255)"},
+  ]
+
+  return output;
+}
+
+
+
+const lines = await csvToArray(filename);
+
+console.log(identifyTable(lines));
