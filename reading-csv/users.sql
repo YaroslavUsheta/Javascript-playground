efc73d5911fd4d90dd8f597a9666ec9ff0c14968
Dmytro Bevzenko <dbvz@protonmail.com>
2022-12-11 21:21:02 -0600

    Add complete sql to users.sql

--- reading-csv/users.sql
+++ reading-csv/users.sql
@@ -5,5 +5,13 @@ CREATE TABLE Users (
     Title varchar(255)
 );
 
-INSERT INTO table_name (UserID, LastName, FirstName, Title)
-VALUES (1, 'Hunter', 'Scott', 'VP of Azure Development')
+INSERT INTO Users (UserID, LastName, FirstName, Title)
+VALUES (1, 'Hunter', 'Scott', 'VP of Azure Development');
+
+INSERT INTO Users (UserID, LastName, FirstName, Title)
+VALUES (2, 'Guthrie', 'Scott', 'EVP of Azure & AI');
+
+INSERT INTO Users (UserID, LastName, FirstName, Title)
+VALUES (3, 'Hanselman', 'Scott', 'Director of Azure');
+
+select LastName, Title from Users where FirstName = 'Scott'
